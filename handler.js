a82a7f6b9c8f4f3ccbaa252128463048f0bada59
Serverlessp <pedrotavares.ms@gmail.com>
2020-04-27 07:08:44 +0100

    Initial commit

---
+++ handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'I am an Hello World type function!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
